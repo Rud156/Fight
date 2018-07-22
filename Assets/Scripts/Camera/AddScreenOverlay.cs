d5b682e627934f9747a6433f68db760f54000d98
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-22 19:35:53 +0530

    Added Screen Sound Overlay

---
+++ Assets/Scripts/Camera/AddScreenOverlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityEngine.PostProcessing.Utilities;
+using UnityEngine.UI;
+
+public class AddScreenOverlay : MonoBehaviour
+{
+
+    [Header("Chromatic Aberration Texture")]
+    public Texture2D overlayTexture;
+
+    [Header("Post Processing Stack")]
+    public PostProcessingController controller;
+
+    public void TurnOnChromaticAberration()
+    {
+        controller.enableChromaticAberration = true;
+        controller.chromaticAberration.spectralTexture = overlayTexture;
+        controller.chromaticAberration.intensity = 1;
+    }
+
+    public void TurnOffChromaticAberration()
+    {
+        controller.enableChromaticAberration = false;
+    }
+}
