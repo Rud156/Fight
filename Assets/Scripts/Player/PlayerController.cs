ec25d25fdec592eb6788138d3149080d2ec29104
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-24 09:35:34 +0530

    Adding Player Animation Controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    private int currentAttackState;
+    private Rigidbody playerRB;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody>();
+        currentAttackState = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
