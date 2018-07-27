00e8e08d84d3eb8b977ad96dd4c481b474363f82
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-27 22:38:43 +0530

    Added Mist Effect

---
+++ Assets/Scripts/Effects/SpawnSparksEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSparksEffect : MonoBehaviour
+{
+
+    [Header("Sparks Effect")]
+    public GameObject sparksEffect;
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag(TagsManager.SwordContact))
+            return;
+
+        ContactPoint contactPoint = other.contacts[0];
+        Vector3 position = contactPoint.point;
+
+        Instantiate(sparksEffect, position, sparksEffect.transform.rotation);
+    }
+}
--- Assets/Scripts/Static Content/TagsManager.cs
+++ Assets/Scripts/Static Content/TagsManager.cs
@@ -10,4 +10,6 @@ public class TagsManager
     public const string CollisionSpawn = "Collision Spawn";
     public const string JumpGround = "Jump Ground";
     public const string PlayerBody = "Player Body";
+    public const string SwordContact ="Sword Contact";
+    public const string FootContact = "Foot Contact";
 }
