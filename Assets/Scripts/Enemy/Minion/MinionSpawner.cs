15069ca382e42eee165d710a0d7bec9009e56cf7
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-31 18:23:30 +0530

    Added Basic Spawner

---
+++ Assets/Scripts/Enemy/Minion/MinionSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionSpawner : MonoBehaviour
+{
+
+    public BoxCollider enclosingBoxCollider;
+    public GameObject minion;
+    public float spawnTime = 7f;
+
+    private Coroutine coroutine;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartSpawn();
+    }
+
+    public void StartSpawn()
+    {
+        coroutine = StartCoroutine(SpawnMinions());
+
+    }
+
+    public void StopSpawn()
+    {
+
+        StopCoroutine(coroutine);
+    }
+
+    IEnumerator SpawnMinions()
+    {
+        while (true)
+        {
+            Vector3 randomPoint = new Vector3(
+                Random.Range(-enclosingBoxCollider.bounds.extents.x, enclosingBoxCollider.bounds.extents.x),
+                0,
+                Random.Range(-enclosingBoxCollider.bounds.extents.z, enclosingBoxCollider.bounds.extents.z)
+            ) + enclosingBoxCollider.bounds.center;
+
+            Instantiate(minion, randomPoint, minion.transform.rotation);
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
