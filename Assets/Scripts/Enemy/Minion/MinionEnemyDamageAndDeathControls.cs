81ffb82c2d1135af7840412a5528445ac4e6bb13
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-31 10:47:27 +0530

    Added Minion Enemy Prefab

---
+++ Assets/Scripts/Enemy/Minion/MinionEnemyDamageAndDeathControls.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinionEnemyDamageAndDeathControls : MonoBehaviour
+{
+
+    [Header("Minion Stats")]
+    public float maxMinionHealth = 30;
+    public GameObject deathEffect;
+
+    [Header("Minion Health")]
+    public Color minHealthColor = Color.red;
+    public Color halfHealthColor = Color.yellow;
+    public Color maxHealthColor = Color.green;
+    public Slider minionHealthSlider;
+    public Image minionHealthFiller;
+
+    private float currentMinionHealth;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        currentMinionHealth = maxMinionHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float maxHealth = maxMinionHealth;
+        float currentHealthLeft = currentMinionHealth;
+        float healthRatio = currentHealthLeft / maxHealth;
+        if (healthRatio <= 0.5)
+            minionHealthFiller.color = Color.Lerp(minHealthColor, halfHealthColor, healthRatio * 2);
+        else
+            minionHealthFiller.color = Color.Lerp(halfHealthColor, maxHealthColor, (healthRatio - 0.5f) * 2);
+        minionHealthSlider.value = healthRatio;
+
+        if (currentHealthLeft <= 0)
+        {
+            Instantiate(deathEffect, gameObject.transform.position, deathEffect.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(TagsManager.SwordContact))
+            currentMinionHealth -= Random.Range(20, 40);
+
+        if (other.CompareTag(TagsManager.FootContact))
+            currentMinionHealth -= Random.Range(10, 20);
+    }
+}
