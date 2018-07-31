5d80ec5a3f6a0a7a25ea6a25e50bc137582a4b82
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-07-31 09:02:19 +0530

    Adding Enemies

---
+++ Assets/Scripts/Enemy/MoveTowardsPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+
+    private GameObject player;
+
+    private NavMeshAgent agent;
+    private Animator enemyAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(TagsManager.Player);
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        enemyAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyAnimator.SetFloat(EnemyControlsManager.EnemyVelocity, agent.velocity.magnitude);
+        agent.SetDestination(player.transform.position);
+    }
+}
