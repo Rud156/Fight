a7433ace9ccc8e667acd0faff3abeb5b25d2c484
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-04 22:45:26 +0530

    Updated Boss Enemy Animation Controller

--- Assets/Scripts/Enemy/Minion/MinionSpawner.cs
+++ Assets/Scripts/Enemy/Minion/MinionSpawner.cs
@@ -16,12 +16,6 @@ public class MinionSpawner : MonoBehaviour
 
     private Coroutine coroutine;
 
-    // Use this for initialization
-    void Start()
-    {
-        StartSpawn();
-    }
-
     public void StartSpawn()
     {
         coroutine = StartCoroutine(SpawnMinions());
